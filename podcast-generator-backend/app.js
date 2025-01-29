794cd298fa5af48d28961a08e2b05f1c8489343c
incognito1025 <nkhanamart@gmail.com>
2025-01-29 06:47:41 -0500

    update app.js to reflect syntax corrections and correct file and folder paths

--- podcast-generator-backend/app.js
+++ podcast-generator-backend/app.js
@@ -1,8 +1,12 @@
+//app.js
 //require/import dependencies
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer"); //for handling file uploads
-const { generatePodcast, generateFromTranscript } = require("./controllers");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { generatePodcast } = require("./controllers/podcastController");
+const { generateFromTranscript } = require("./controllers/transcriptController");
+const { errorHandler } = require("./middleware/errorHandler");
 
 //Create instance of express server app
 const app = express();
@@ -13,12 +17,7 @@ app.use(express.json());
 
 //Health check route
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to the Podcast Generatort");
-});
-
-//Error route
-app.get("*", (req, res) => {
-  res.status(404).send("This is the error page.");
+  res.status(200).send("Welcome to the Podcast Generator");
 });
 
 //POST route for audio file processing
@@ -26,6 +25,14 @@ app.post("/api/generate-podcast", multer().single("audio"), generatePodcast);
 
 //POST route for text processing
 app.post("/api/generate-from-transcript", generateFromTranscript);
+
+//Error route
+app.get("*", (req, res) => {
+  res.status(404).send("This is the error page.");
+});
+
+//error handler middleware
+app.use(errorHandler);
 
 //Export server app
 module.exports = app;
